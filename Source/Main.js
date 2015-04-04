95067a74e8155c7d005db99f2cb4d8a52bcce1fd
steelbrain <aneesiqbalbhatti@gmail.com>
2015-04-04 03:57:36 -0700

    :new: Implement Proto::Decode

--- Source/Main.js
+++ Source/Main.js
@@ -15,8 +15,35 @@ class Proto{
     });
     return ToReturn.join("\r\n");
   }
-  static Decode(){
+  static Decode(Content, ReturnOffset){
+    let Type = Content.substr(0, 1);
+    let Offset = Content.indexOf("\r\n");
+    let Count = parseInt(Content.substr(1, Offset));
+    let ToReturn = null;
+    let Temp = null;
 
+    if(Type === '*'){
+      ToReturn = [];
+      Temp = {Offset: Offset};
+      for(let i = 0; i < Count; ++i){
+        let RemovedLength = Offset + 2;
+        Temp = Proto.Decode(Content.substr(Offset + 2), true);
+        Offset = Temp.Offset + RemovedLength;
+        ToReturn.push(Temp.Content);
+      }
+    } else if(Type === '$') {
+      ToReturn = Content.substr(Offset + 2, Count);
+      Offset += 2 + Count;
+    } else if(Type === ':'){
+      Offset += 2 + Count.length;
+      ToReturn = parseInt(Count);
+    } else if(Type === '-'){
+      throw new Error(Content.substr(Offset + 2, Count));
+    }
+    if(ReturnOffset){
+      return {Offset: Offset, Content: ToReturn};
+    }
+    return ToReturn;
   }
 }
 module.exports = Proto;
