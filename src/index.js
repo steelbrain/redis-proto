6837722977306bbd0d4232f1d9e7718ded643d45
Steel Brain <i@steelbrain.me>
2017-03-10 02:55:45 +0500

    :new: Implement progressive decode

--- src/index.js
+++ src/index.js
@@ -25,6 +25,80 @@ export function encode(request: any, encodeArray: boolean = true) {
   return `$${value.length}\r\n${value}\r\n`
 }
 
+export function decodeProgressive(content: Buffer, startIndex: number): { index: number, value: any } {
+  let currentIndex = startIndex
+  const type = content.toString('utf8', currentIndex, currentIndex + 1)
+  // +1 because type takes 1 character
+  currentIndex++
+
+  if (type === '*') {
+    // Array
+    const lengthEnd = content.indexOf('\r\n', currentIndex)
+    const length = parseInt(content.toString('utf8', currentIndex, lengthEnd), 10)
+    // +2 because of \r\n after length ends
+    currentIndex = lengthEnd + 2
+
+    const value = []
+    for (let i = 0; i < length; i++) {
+      const entry = decodeProgressive(content, currentIndex)
+      currentIndex = entry.index
+      value.push(entry.value)
+    }
+    return { index: currentIndex, value }
+  }
+
+  if (type === '$') {
+    // String or Null
+    const lengthEnd = content.indexOf('\r\n', currentIndex)
+    const length = parseInt(content.toString('utf8', currentIndex, lengthEnd), 10)
+    // +2 because of \r\n after length ends
+    currentIndex = lengthEnd + 2
+
+    let value
+    if (length === -1) {
+      // Null
+      value = null
+    } else {
+      // String
+      value = content.toString('utf8', currentIndex, currentIndex + length)
+      // +2 because of \r\n at the end of string
+      currentIndex += length + 2
+    }
+
+    return { index: currentIndex, value }
+  }
+
+  if (type === '+') {
+    // Simple string
+    const valueEnd = content.indexOf('\r\n', currentIndex)
+    const value = content.toString('utf8', currentIndex, valueEnd)
+    // +2 because of \r\n at the end of simple string
+    currentIndex = valueEnd + 2
+
+    return { index: currentIndex, value }
+  }
+
+  if (type === ':') {
+    // Integer
+    const valueEnd = content.indexOf('\r\n', currentIndex)
+    const value = parseInt(content.toString('utf8', currentIndex, valueEnd), 10)
+    // +2 because of \r\n at the end of simple string
+    currentIndex = valueEnd + 2
+
+    return { index: currentIndex, value }
+  }
+
+  if (type === '-') {
+    // Error
+    const valueEnd = content.indexOf('\r\n', currentIndex)
+    const value = content.toString('utf8', currentIndex, valueEnd)
+    throw new Error(value)
+  }
+
+  throw new Error('Malformed Input')
+}
+console.log(decodeProgressive(Buffer.from('*5\r\n$3\r\nfoo\r\n$3\r\nbar\r\n$-1\r\n+OK\r\n:9999\r\n'), 0))
+
 export function decode(content) {
   const bufferContent = Buffer.isBuffer(content) ? content : new Buffer(content)
   const Buffers = []
